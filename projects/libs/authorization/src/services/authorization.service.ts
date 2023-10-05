7709baf042a277ddd157c381f70d6313c69a17ef
Mikhail <misha.rukovishnikow@yandex.ru>
2023-10-05 22:09:18 +0300

    Add authorization library

---
+++ projects/libs/authorization/src/services/authorization.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizationService {
+  public authorize(login: string, password: string) {
+
+  }
+}
