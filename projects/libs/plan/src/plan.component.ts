4d6fe5d7ee16201dd79ee986d0bfb5c0457b7dbf
mi.rukovishnikov <mi.rukovishnikov@tinkoff.ru>
2023-10-30 01:13:53 +0300

    Fix plan

--- projects/libs/plan/src/plan.component.ts
+++ projects/libs/plan/src/plan.component.ts
@@ -2,6 +2,7 @@ import {Component, Input} from '@angular/core';
 import {map, Observable} from "rxjs";
 import {IncomeDto, OutcomeDto, PlanDto} from "./dtos";
 import {PlanService} from "./services";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'plan',
@@ -11,16 +12,15 @@ import {PlanService} from "./services";
   ]
 })
 export class PlanComponent {
-  @Input({required: true})
-  planId: string = '';
-
   plan$: Observable<PlanDto>;
 
   incomes$: Observable<IncomeDto[]>;
   outcomes$: Observable<OutcomeDto[]>;
 
-  constructor(private readonly planService: PlanService) {
-    this.plan$ = this.planService.getPlan(this.planId);
+  constructor(private readonly planService: PlanService,
+              private readonly route: ActivatedRoute) {
+    const planId = this.route.snapshot.paramMap.get('id') ?? '';
+    this.plan$ = this.planService.getPlan(planId);
 
     this.incomes$ = this.plan$.pipe(map(plan => plan.incomes))
     this.outcomes$ = this.plan$.pipe(map(plan => plan.outcomes))
