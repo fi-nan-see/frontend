43ee19696519db2c64f52e6af7a3d43b70bf186c
Kroexov <kroexov@gmail.com>
2023-10-29 21:38:37 +0300

    add get plans list

---
+++ projects/libs/api/src/lib/dtos/get-plans-response.ts
@@ -0,0 +1,8 @@
+export interface GetPlansResponse{
+  id: string
+  name: string
+  start_date: string
+  end_date: string
+  current_balance: bigint
+  is_actual: boolean
+}
--- projects/libs/api/src/lib/plan-client.ts
+++ projects/libs/api/src/lib/plan-client.ts
@@ -3,6 +3,8 @@ import {CreatePlanResponse} from "./dtos/create-plan-response";
 import {CreatePlanRequest} from "./dtos/create-plan-request";
 import {Observable} from "rxjs";
 import {Injectable} from "@angular/core";
+import {GetPlansResponse} from "./dtos/get-plans-response";
+import {GetPlanRequest} from "./dtos/get-plan-request";
 
 @Injectable()
 export class PlanClient {
@@ -13,5 +15,13 @@ export class PlanClient {
 
   createPlan(request: CreatePlanRequest): Observable<CreatePlanResponse> {
     return this.httpClient.post<CreatePlanResponse>(this.url, request)
+  }
+
+  getPlansList(): Observable<GetPlansResponse>{
+    return this.httpClient.get<GetPlansResponse>(this.url)
+  }
+
+  getPlanById(reuest: GetPlanRequest): Observable<GetPlanResponse>{
+
   }
 }
