5162fc39d7993ec7aaf298618b789fd8eca75543
mi.rukovishnikov <mi.rukovishnikov@tinkoff.ru>
2023-10-30 16:52:47 +0300

    Fix plan

--- projects/libs/plan/src/services/plan.service.ts
+++ projects/libs/plan/src/services/plan.service.ts
@@ -3,7 +3,7 @@ import {PlanDto} from "../dtos";
 import {delay, map, Observable, of, timer} from "rxjs";
 import {PlanClient} from 'projects/libs/api/src/lib/plan-client';
 import {GetPlanRequest} from 'projects/libs/api/src/lib/dtos/get-plan-request';
-import { DeletePlanRequest } from 'projects/libs/api/src/lib/dtos/delete-plan-request';
+import {DeletePlanRequest} from 'projects/libs/api/src/lib/dtos/delete-plan-request';
 
 @Injectable({
   providedIn: 'root'
@@ -12,43 +12,29 @@ export class PlanService {
   constructor(private readonly planClient: PlanClient) {
   }
 
-  // getPlan(id: string): Observable<PlanDto> {
-  //   return this.planClient.getPlanById(new GetPlanRequest(id))
-  //     .pipe(
-  //       map(plan => {
-  //         return {
-  //           id: plan.id,
-  //           name: plan.name,
-  //           startDate: plan.start_date,
-  //           endDate: plan.end_date,
-  //           initialBalance: plan.initial_balance,
-  //           currentBalance: plan.current_balance,
-  //           currentDailyBalance: plan.current_daily_balance ?? 0,
-  //           remains: 0,
-  //           incomes: [],
-  //           outcomes: []
-  //         }
-  //       })
-  //     );
-  // }
-
   getPlan(id: string): Observable<PlanDto> {
-    return of({
-      id: "dsgdsgsd",
-      name: "sdgsdg",
-      startDate: "sdgsd",
-      endDate: "sdgsd",
-      initialBalance: 5,
-      currentBalance: 6,
-      currentDailyBalance: 2,
-      remains: 2,
-      incomes: [],
-      outcomes: []
-    })
+    return this.planClient.getPlanById(new GetPlanRequest(id))
+      .pipe(
+        map(plan => {
+          return {
+            id: plan.id,
+            name: plan.name,
+            startDate: plan.start_date,
+            endDate: plan.end_date,
+            initialBalance: plan.initial_balance,
+            currentBalance: plan.current_balance,
+            currentDailyBalance: plan.current_daily_balance ?? 0,
+            remains: 0,
+            incomes: [],
+            outcomes: []
+          }
+        })
+      );
   }
 
   deletePlan(id: string): Observable<void> {
     return this.planClient.deletePlanById(new DeletePlanRequest(id))
-      .pipe(map(() => {}));
+      .pipe(map(() => {
+      }));
   }
 }
